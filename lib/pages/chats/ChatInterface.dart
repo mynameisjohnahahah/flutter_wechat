897e0e71151951c87e186cdcca8041e627b4b062
lxp <2545343016@qq.com>
2019-02-27 23:53:01 +0800

    'socket连接成功'

--- lib/pages/chats/ChatInterface.dart
+++ lib/pages/chats/ChatInterface.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_socket_io/flutter_socket_io.dart';
-import 'package:flutter_socket_io/socket_io_manager.dart';
+import 'package:socket_flutter_plugin/socket_flutter_plugin.dart';
+import 'dart:async';
 
 class ChatInterface extends StatefulWidget {
   ChatInterface({Key key, @required this.id}):super(key: key);
@@ -11,6 +11,7 @@ class ChatInterface extends StatefulWidget {
 
 class _ChatInterfaceState extends State<ChatInterface> {
   var maxScrollExtent;
+  String _platformVersion = 'Unknown';
 
   ScrollController _controller = new ScrollController();
 
@@ -21,52 +22,25 @@ class _ChatInterfaceState extends State<ChatInterface> {
     super.dispose();
   }
 
-  SocketIO socketIO;
-  void _connectSocket01() { 
-    //update your domain before using  
-    socketIO = SocketIOManager().createSocketIO("https://119.29.119.87:8765", "", query: "userId=21031", socketStatusCallback: _socketStatus); 
-
-    //call init socket before doing anything 
-    socketIO.init(); 
-
-    //subscribe event
-    // socketIO.subscribe("socket_info", _onSocketInfo); 
-
-    //connect socket 
-    socketIO.connect(); 
-  }
-
-  _socketStatus(dynamic data) { 
-	print("测试测试测试测试: " + data); 
-}
-
-  // _subscribes() { 
-  //   if (socketIO != null) { 
-  //     socketIO.subscribe("chat_direct", _onReceiveChatMessage); 
-  //   } 
-  // }
-
-  // void _onReceiveChatMessage(dynamic message) { 
-  //   print("Message from UFO: " + message); 
-  // }
-
-  // void _sendChatMessage(String msg) async { 
-  //   if (socketIO != null) { 
-  //     String jsonData = '{"message":{"type":"Text","content": ${(msg != null && msg.isNotEmpty) ? '"${msg}"' : '"Hello SOCKET IO PLUGIN :))"'},"owner":"589f10b9bbcd694aa570988d","avatar":"img/avatar-default.png"},"sender":{"userId":"589f10b9bbcd694aa570988d","first":"Ha","last":"Test 2","location":{"lat":10.792273999999999,"long":106.6430356,"accuracy":38,"regionId":null,"vendor":"gps","verticalAccuracy":null},"name":"Ha Test 2"},"receivers":["587e1147744c6260e2d3a4af"],"conversationId":"589f116612aa254aa4fef79f","name":null,"isAnonymous":null}'; 
-  //     socketIO.sendMessage("chat_direct", jsonData, _onReceiveChatMessage); 
-  //   }
-  // }
-
-  // _destroySocket() { 
-  //   if (socketIO != null) { 
-  //     SocketIOManager().destroySocket(socketIO); 
-  //   } 
-  // }
-
   void initState() {
     super.initState();
-
-    _connectSocket01();
+    initPlatformState();
+  }
+  
+   Future<void> initPlatformState() async {
+    try {
+      SocketFlutterPlugin myIO = new SocketFlutterPlugin();
+      myIO.socket("http://119.29.119.87:8765/");
+      myIO.connect();
+      String jsonData =
+              '{"content":"test"}';
+      myIO.emit("send",jsonData);
+      myIO.on("getMsg",(data){
+        debugPrint(data.toString());
+      });
+    } catch(error) {
+      _platformVersion = 'Failed to get platform version.';
+    }
   }
 
   Widget buildTextField(TextEditingController controller) {
